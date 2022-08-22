560174c27934c1a68aaa2c321d3e10f3dcff7e6e
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-08-22 22:56:59 +0200

    initial env

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from selenium import webdriver
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import pandas as pd
+from urllib.request import urlopen
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+url = "https://www.flipkart.com/laptops/~buyback-guarantee-on-laptops-/pr?sid=6bo%2Cb5g&amp;amp;amp;amp;amp;amp;amp;amp;amp;amp;uniq"
+
+
+def main():
+    page = urlopen(url)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
+    print(soup.find('a'))
+    products = []  # List to store name of the product
+    prices = []  # List to store price of the product
+    ratings = []  # List to store rating of the product
+
+    print(products)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
