7817fb2f32b235349888cd49b91ad254dc0c0021
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-09-05 18:41:01 +0200

    tried supervisor but it needs Unix

--- movies_api/management/commands/my_command.py
+++ movies_api/management/commands/my_command.py
@@ -3,12 +3,15 @@ from parser import Parser
 from movies_api.models import *
 from movies_api.tasks import demo_task
 from background_task.models import Task, CompletedTask
+from django.core.management import call_command
 
 
 class Command(BaseCommand):
     def handle(self, **options):
         Task.objects.all().delete()
-        demo_task("Hi after 60 min", repeat=5)
+        # daily would make it happen one time
+        demo_task("Hi after 60 min", repeat=Task.DAILY)
+
         # MovieItem.objects.all().delete()
         # CinemaItem.objects.all().delete()
         # Parser("https://elcinema.com/en/theater/1").parse_cinames()
