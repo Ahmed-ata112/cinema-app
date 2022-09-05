c9a06141048af3f134841b4a5361109dff14ab94
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-09-05 13:02:18 +0200

    added background Tasks and downgrading django to 3.2

--- LOLYApp/settings.py
+++ LOLYApp/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'background_task',
     'movies_api',
 ]
 
@@ -129,8 +130,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
