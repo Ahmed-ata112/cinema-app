fe0ae3cc8669ac8725883d420e004735705c2e6f
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-09-26 17:13:13 +0200

    basic search

--- movies_api/views.py
+++ movies_api/views.py
@@ -1,3 +1,4 @@
+from re import search
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.settings import api_settings
@@ -5,6 +6,8 @@ from movies_api import serializers
 from movies_api import models
 from rest_framework.response import Response
 from rest_framework import permissions
+from rest_framework import filters
+import django_filters.rest_framework
 
 
 class MovieFeedViewSet(viewsets.ModelViewSet):
@@ -12,7 +15,11 @@ class MovieFeedViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = serializers.MovieItemSerializer
     queryset = models.MovieItem.objects.all()
-    
+    search_fields = ['movie_title', 'movie_genre', ]
+    filterset_fields = ['movie_genre', 'movie_rating']
+    filter_backends = (filters.SearchFilter,
+                       django_filters.rest_framework.DjangoFilterBackend)
+
 
 class CinemaFeedViewSet(viewsets.ModelViewSet):
     '''Handle creating ,Reading and updating profile feed item'''
