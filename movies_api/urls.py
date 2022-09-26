130049ef2a2fcee5d38011ddbbbc0b8457eef2d6
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-09-27 01:22:53 +0200

    genres

--- movies_api/urls.py
+++ movies_api/urls.py
@@ -6,7 +6,7 @@ from movies_api import views
 router = DefaultRouter()
 router.register('movies', views.MovieFeedViewSet)
 router.register('cinemas', views.CinemaFeedViewSet)
-
+router.register('genres', views.GenresViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
