33c77669101a378e8a21eea3b3c7b9e8831a6ce6
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2022-09-29 01:39:44 +0200

    limited Search results to 5 results

--- movies_api/views.py
+++ movies_api/views.py
@@ -9,6 +9,7 @@ from rest_framework import permissions
 from movies_api.filters import MoviesFilter
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.pagination import PageNumberPagination
 
 
 class MovieFeedViewSet(viewsets.ModelViewSet):
@@ -22,13 +23,19 @@ class MovieFeedViewSet(viewsets.ModelViewSet):
     filterset_class = MoviesFilter
 
 
+class CustomSearchFilter(filters.SearchFilter):
+    def filter_queryset(self, request, queryset, view):
+        # to Return only the first 5 results in Search results
+        return super().filter_queryset(request, queryset, view)[:5]
+
+
 class CinemaFeedViewSet(viewsets.ModelViewSet):
     '''Handle creating ,Reading and updating profile feed item'''
 
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = serializers.CinemaItemSerializer
     queryset = models.CinemaItem.objects.all()
-    filter_backends = (filters.SearchFilter,)
+    filter_backends = (CustomSearchFilter,)
     search_fields = ['cinema_name', ]
 
 
